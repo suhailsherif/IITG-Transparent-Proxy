da7c12caddaac83be2161406dda44abc34cd3b12
Kunal Khandelwal <kunal15595@gmail.com>
2015-02-08 11:15:00 +0530

    logging to separate file

--- start.sh
+++ start.sh
@@ -4,27 +4,59 @@ sudo echo "This is the easy version of starting up the transparent proxy."
 echo "The backend needs to compiled. I've done that in the script itself."
 echo "In case make fails, you will need to compile libevent. I've included the tar for it. Extract it and use the readme file. "
 
+echo "Removing log files ..."
+sudo rm -rf *.log &
+echo "Log files removed"
+
+echo "Checking Internet connection ..."
+if ping -W 5 -c 1 202.141.80.3 >/dev/null; then
+    echo "Internet is up."
+else
+    echo "Offline"
+    echo "Check Internet Access and try again"
+    echo "Exit on Error !"
+    exit
+fi
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' libevent-dev |grep "install ok installed")
+echo Checking for libevent-dev: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No libevent-dev. Setting up libevent-dev."
+  sudo apt-get --force-yes --yes install libevent-dev
+fi
+
 sudo fuser -k 55/udp
 sleep 0.1
 
 if [ ! -f ./redsocks/redsocks ] 
 then
+	echo "make redsocks initiating ..."
 	make -C ./redsocks/
+fi
+
+if [ ! -f ./redsocks/redsocks ] 
+then
+	echo "Could not compile redsocks."
+	echo "Exit On Error !"
+	exit
+else
+	echo "make redsocks successful"
 fi
 
 if [ ! -f ./config.sh ] 
 then
+	echo "Creating file config.sh ..."
 	touch config.sh
 	cp config_bak.sh config.sh
+else
+	echo -n "read from config file ?[y/n] : "
+	read -p "" choice
 fi
 
 chmod +rwx config.sh
 
 vpnbook_cred_path="/vpnbook_cred"
 proxy_cred_path="/proxy_cred"
-
-echo -n "read from config file ?[y/n] : "
-read -p "" choice
 
 if [ "$choice" = "y" ]
 then
@@ -40,6 +72,14 @@ else
 	stty echo
 	read -p "VPNBook username ? : " vpnbook_username
 	read -p "VPNBook password ? : " vpnbook_password
+	for f in ./*.ovpn; do
+	    [ -e "$f" ] && echo "files do exist" || echo "files do not exist"
+	    echo "Do you want to use $f as server configuration file ?[y/n] :"
+	    read -p "" ch
+
+	    ## This is all we needed to know, so we can break after the first iteration
+	    break
+	done
 	read -p "VPN config file path(full) ? : " vpnbook_path
 	echo -n "\nDo you want to save your configuration ?[y/n] : "
 	read -p "" save
@@ -56,19 +96,27 @@ else
 	fi
 fi
 
+echo "Creating temporary files for openvpn connection ..."
 echo "$vpnbook_username\n$vpnbook_password" > $(pwd)$vpnbook_cred_path
 echo "$proxy_username\n$proxy_password" > $(pwd)$proxy_cred_path
 
 
 ./script start & 
-./redsocksConfig.sh $proxy_server $proxy_port $proxy_username $proxy_password & 
-sudo python fakeDNS.py &
+echo "Configuring redsocks ..."
+./redsocksConfig.sh $proxy_server $proxy_port $proxy_username $proxy_password > ./log/Redsocks.log 2>&1 & 
+echo "redsocks configured successfully"
+echo "initiating fake DNS server ..."
+sudo python fakeDNS.py > ./log/DNS &
+echo "DNS server initiated"
 echo $! > pidfile.temp &
 if [ -f ./vpnbook_cred ] && [ -f ./proxy_cred ] 
 then
+	echo "initiating openvpn connection ..."
 	openvpn --config $vpnbook_path --auth-user-pass $(pwd)$vpnbook_cred_path --http-proxy $proxy_server $proxy_port $(pwd)$proxy_cred_path basic &
 	sleep 0.1
+	echo "removing temporary credential files ..."
 	sudo rm -rf ./proxy_cred ./vpnbook_cred &
+	echo "credential files removed"
 else
 	echo "config files not found"
 	echo "\n\nExit On Error !\n\n"
